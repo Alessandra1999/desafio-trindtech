6af42ac24671470961c6d09db6b84cc1e825bd17
Alessandra Bombardi <ale.brbr@gmail.com>
2024-10-15 16:01:43 -0300

    Now the student can be associated with more than one course at a time

--- frontend-trindtech/src/components/Forms/CourseForm.jsx
+++ frontend-trindtech/src/components/Forms/CourseForm.jsx
@@ -4,168 +4,212 @@ import { MdAddCircleOutline } from "react-icons/md";
 import { getCourses } from "../../services/apiService";
 
 const CustomForm = styled.form`
-    margin-top: 62px;
+  margin-top: 62px;
 `;
 
 const Container = styled.div`
-    max-width: 1098px;
-    height: auto;
-    font-family: "Montserrat", sans-serif;
-    font-weight: 500;
-    margin-bottom: 50px;
+  max-width: 1098px;
+  height: auto;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 500;
+  margin-bottom: 50px;
 `;
 
 const Title = styled.h4`
-    margin-bottom: 25px;
-    font-size: 22px;
-    font-weight: 500;
+  margin-bottom: 25px;
+  font-size: 22px;
+  font-weight: 500;
 `;
 
 const CourseRow = styled.div`
   display: flex;
-  margin-top: ${(props) => (props.index > 0 ? "26px" : "0px")}; /* Condicional para adicionar margin-top somente nas novas linhas */
+  margin-top: ${(props) =>
+    props.index > 0
+      ? "26px"
+      : "0px"}; /* Condicional para adicionar margin-top somente nas novas linhas */
 `;
 
 const CustomInput = styled.input`
-    background-color: #FFF;
+  background-color: #fff;
 
-    &:focus {
-        background-color: #FFF;
-        outline: none;
-        border-color: black;
-        box-shadow: none;
-    }
+  &:focus {
+    background-color: #fff;
+    outline: none;
+    border-color: black;
+    box-shadow: none;
+  }
 `;
 
 const CustomSelect = styled.select`
-    background-color: #F2F2F2;
+  background-color: #f2f2f2;
 
-    &:focus {
-        background-color: #F2F2F2;
-        outline: none;
-        border-color: black;
-        box-shadow: none;
-    }
+  &:focus {
+    background-color: #f2f2f2;
+    outline: none;
+    border-color: black;
+    box-shadow: none;
+  }
 `;
 
 const CustomButton = styled.button`
-    border: none;
-    background-color: transparent;
-    font-size: 24px;
-    margin-left: 10px;
+  border: none;
+  background-color: transparent;
+  font-size: 24px;
+  margin-left: 10px;
 
-    &:hover {
-        cursor: pointer;
-    }
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
-function CourseForm({ studentCourseData, courseData, setCourseData, setStudentCourseData }) {
-    const [courses, setCourses] = useState([{ courseName: "", conclusionDate: "" }]);
-    const [availableCourses, setAvailableCourses] = useState([]);
+function CourseForm({
+  studentCourseData,
+  courseData,
+  setCourseData,
+  setStudentCourseData,
+}) {
+  const [courses, setCourses] = useState([
+    { courseName: "", conclusionDate: "" },
+  ]);
+  const [availableCourses, setAvailableCourses] = useState([]);
 
+  useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        const courses = await getCourses();
+        setAvailableCourses(courses);
+      } catch (error) {
+        console.error("Erro ao buscar cursos:", error);
+      }
+    };
+    fetchCourses();
+  }, []);
 
-    useEffect(() => {
-        const fetchCourses = async () => {
-            try {
-                const courses = await getCourses();
-                setAvailableCourses(courses);
-            } catch (error) {
-                console.error('Erro ao buscar cursos:', error);
-            }
+  // Função para adicionar novos campos de input
+  const handleAddCourse = () => {
+    setCourses([...courses, { courseName: "", conclusionDate: "" }]);
+  };
+
+  const handleInputChange = (index, id_course, value) => {
+    const newCourses = [...courses];
+    newCourses[index][id_course] = value;
+    setCourses(newCourses);
+  };
+
+  const handleCourseSelect = (index, selectedOption) => {
+    console.log("Selected course ID: ", selectedOption);
+    const selectedCourse = availableCourses.find((course) => {
+      console.log(
+        "Comparing: ",
+        course.id_course,
+        " (type: ",
+        typeof course.id_course,
+        ") with ",
+        Number(selectedOption),
+        " (type: ",
+        typeof Number(selectedOption),
+        ")"
+      );
+      return course.id_course == Number(selectedOption);
+    });
+
+    if (selectedCourse) {
+      console.log("Selected course data: ", selectedCourse);
+      setCourseData((prevCourses) => {
+        const updatedCourses = [...prevCourses];
+        updatedCourses[index] = {
+          id_course: selectedCourse.id_course,
+          course_name: selectedCourse.course_name,
         };
-        fetchCourses();
-    }, []);
 
-    // Função para adicionar novos campos de input 
-    const handleAddCourse = () => {
-        setCourses([...courses, { courseName: "", conclusionDate: "" }]);
-    };
+        console.log("Updated course data: ", updatedCourses); // Verifique o array atualizado
+        return updatedCourses;
+      });
+    } else {
+      console.error("No course found for selected ID");
+    }
+  };
 
-    const handleInputChange = (index, id_course, value) => {
-        const newCourses = [...courses];
-        newCourses[index][id_course] = value;
-        setCourses(newCourses);
-    };
+  // Função para atualizar a data de conclusão para um curso específico
+  const handleChange = (index, e) => {
+    const { name, value } = e.target;
+    // Verifica se o studentCourseData é um array
+    setStudentCourseData((prevData) => {
+      const newStudentCourseData = [...prevData]; // Faz uma cópia do array atual
+      newStudentCourseData[index] = {
+        ...newStudentCourseData[index],
+        [name]: value,
+      }; // Atualiza a data de conclusão do índice específico
+      return newStudentCourseData; // Retorna o novo array
+    });
+  };
 
-    const handleCourseSelect = (index, selectedOption) => {
-        console.log("Selected course ID: ", selectedOption);
-        const selectedCourse = availableCourses.find(course => {
-            console.log("Comparing: ", course.id_course, " (type: ", typeof course.id_course, ") with ", Number(selectedOption), " (type: ", typeof Number(selectedOption), ")");
-            return course.id_course == Number(selectedOption);
-        });
-        if (selectedCourse) {
-            console.log("Selected course data: ", selectedCourse);
-            setCourseData({
-                id_course: selectedCourse.id_course,
-                course_name: selectedCourse.course_name
-            });
-        }
-    };
-
-    // Função para fazer a implementação juntamente com o Layout
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setStudentCourseData((prev) => ({ ...prev, [name]: value }));
-    };
-
-    return (
-        <CustomForm>
-            <Container className="container">
-                <Title>Cursos</Title>
-                {courses.map((courseData, index) => (
-                    <CourseRow className="row" key={index} index={index}>
-                        <div className="form-group col-md-8">
-                            <label htmlFor={`courseSelect${index}`}>Nome do Curso</label>
-                            <CustomSelect
-                                className="form-control"
-                                id={`courseSelect${index}`}
-                                name="course_name"
-                                value={courseData.id_course}
-                                onChange={(e) => {
-                                    console.log("onChange disparado, valor selecionado: ", e.target.value);  // Verificação
-                                    handleInputChange(index, "id_course", e.target.value);  // Mude para 'id_course'
-                                    handleCourseSelect(index, e.target.value);  // Passe o ID para o handleCourseSelect
-                                }}
-                            >
-                                <option value="">Selecione um curso</option>
-                                {availableCourses.length > 0 ? (
-                                    availableCourses.map((course) => (
-                                        <option key={course.id_course} value={course.id_course}>
-                                            {course.course_name}
-                                        </option>
-                                    ))
-                                ) : (
-                                    <option value="">Nenhum curso disponível</option>
-                                )}
-                            </CustomSelect>
-                        </div>
-                        <div className="form-group col-md-4 d-flex align-items-center">
-                            <div className="w-100">
-                                <label htmlFor={`conclusionInput${index}`}>Data de Conclusão</label>
-                                <div className="d-flex">
-                                    <CustomInput
-                                        type="date"
-                                        className="form-control"
-                                        id={`conclusionInput${index}`}
-                                        name="conclusion_date"
-                                        value={studentCourseData.conclusionDate}
-                                        onChange={(e) => {
-                                            handleInputChange(index, "conclusionDate", e.target.value);
-                                            handleChange(e);
-                                        }}
-                                    />
-                                    <CustomButton type="button" onClick={handleAddCourse}>
-                                        <MdAddCircleOutline />
-                                    </CustomButton>
-                                </div>
-                            </div>
-                        </div>
-                    </CourseRow>
-                ))}
-            </Container>
-        </CustomForm>
-    );
-};
-
+  return (
+    <CustomForm>
+      <Container className="container">
+        <Title>Cursos</Title>
+        {courses.map((courseData, index) => (
+          <CourseRow className="row" key={index} index={index}>
+            <div className="form-group col-md-8">
+              <label htmlFor={`courseSelect${index}`}>Nome do Curso</label>
+              <CustomSelect
+                className="form-control"
+                id={`courseSelect${index}`}
+                name="course_name"
+                value={courseData.id_course}
+                onChange={(e) => {
+                  console.log(
+                    "onChange disparado, valor selecionado: ",
+                    e.target.value
+                  ); // Verificação
+                  handleInputChange(index, "id_course", e.target.value); // Mude para 'id_course'
+                  handleCourseSelect(index, e.target.value); // Passe o ID para o handleCourseSelect
+                }}
+              >
+                <option value="">Selecione um curso</option>
+                {availableCourses.length > 0 ? (
+                  availableCourses.map((course) => (
+                    <option key={course.id_course} value={course.id_course}>
+                      {course.course_name}
+                    </option>
+                  ))
+                ) : (
+                  <option value="">Nenhum curso disponível</option>
+                )}
+              </CustomSelect>
+            </div>
+            <div className="form-group col-md-4 d-flex align-items-center">
+              <div className="w-100">
+                <label htmlFor={`conclusionInput${index}`}>
+                  Data de Conclusão
+                </label>
+                <div className="d-flex">
+                  <CustomInput
+                    type="date"
+                    className="form-control"
+                    id={`conclusionInput${index}`}
+                    name="conclusion_date"
+                    value={courses[index].conclusionDate}
+                    onChange={(e) => {
+                      handleInputChange(
+                        index,
+                        "conclusionDate",
+                        e.target.value
+                      );
+                      handleChange(index, e);
+                    }}
+                  />
+                  <CustomButton type="button" onClick={handleAddCourse}>
+                    <MdAddCircleOutline />
+                  </CustomButton>
+                </div>
+              </div>
+            </div>
+          </CourseRow>
+        ))}
+      </Container>
+    </CustomForm>
+  );
+}
 
 export default CourseForm;
--- frontend-trindtech/src/components/Layout/LayoutForm.jsx
+++ frontend-trindtech/src/components/Layout/LayoutForm.jsx
@@ -1,190 +1,226 @@
-import { useState } from 'react';
-import DynamicHeader from '../Header/DynamicHeader';
-import StudentForm from '../Forms/StudentForm';
-import LocationForm from '../Forms/LocationForm';
-import CourseForm from '../Forms/CourseForm';
-import { createStudent, createLocation, createStudentCourse } from "../../services/apiService";
-import { deleteAllStudentData } from '../../services/apiService';
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import styled from 'styled-components';
+import { useState } from "react";
+import DynamicHeader from "../Header/DynamicHeader";
+import StudentForm from "../Forms/StudentForm";
+import LocationForm from "../Forms/LocationForm";
+import CourseForm from "../Forms/CourseForm";
+import {
+  createStudent,
+  createLocation,
+  createStudentCourse,
+} from "../../services/apiService";
+import { deleteAllStudentData } from "../../services/apiService";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import styled from "styled-components";
 
 const CustomButton = styled.button`
-    background-color: #EA394E;
-    color: #F2F2F2;
-    font-weight: 700;
-    margin-bottom: 30px;
+  background-color: #ea394e;
+  color: #f2f2f2;
+  font-weight: 700;
+  margin-bottom: 30px;
 
-    &:hover {
-        background-color: #c7293c;
-        color: #F2F2F2;
-    }
+  &:hover {
+    background-color: #c7293c;
+    color: #f2f2f2;
+  }
 `;
 
 function LayoutForm() {
-    const [studentData, setStudentData] = useState({
-        student_name: '',
-        student_lastname: '',
-        student_birthdate: '',
-        student_cpf: '',
-        student_gender: '',
-        student_email: '',
-        student_register_date: ''
-    });
+  const [studentData, setStudentData] = useState({
+    student_name: "",
+    student_lastname: "",
+    student_birthdate: "",
+    student_cpf: "",
+    student_gender: "",
+    student_email: "",
+    student_register_date: "",
+  });
 
-    const [locationData, setLocationData] = useState({
-        cep: '',
-        country: '',
-        street: '',
-        district: '',
-        number: '',
-        complement: '',
-        city: '',
-        state: ''
-    });
+  const [locationData, setLocationData] = useState({
+    cep: "",
+    country: "",
+    street: "",
+    district: "",
+    number: "",
+    complement: "",
+    city: "",
+    state: "",
+  });
 
-    const [courseData, setCourseData] = useState({
-        id_course: '',
-        course_name: ''
-    });
+  const [courseData, setCourseData] = useState([
+    {
+      id_course: "",
+      course_name: "",
+    },
+  ]);
 
-    const [studentCourseData, setStudentCourseData] = useState({
-        conclusion_date: ''
-    });
+  const [studentCourseData, setStudentCourseData] = useState([
+    {
+      conclusion_date: "",
+    },
+  ]);
 
-    const [studentId, setStudentId] = useState(null);
-    const [emailValid, setEmailValid] = useState(true);
+  const [studentId, setStudentId] = useState(null);
+  const [emailValid, setEmailValid] = useState(true);
 
-    const validateEmail = (email) => {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailRegex.test(email);
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(studentData.student_email)) {
+      toast.error("Por favor, insira um e-mail válido antes de enviar."); // Mensagem de erro
+      return; // Impede o envio se o e-mail for inválido
+    }
+
+    // Registrar a data atual no formato ISO
+    const currentDate = new Date().toISOString();
+
+    // Atualizar o estado com a data de registro do aluno
+    const updatedStudentData = {
+      ...studentData,
+      student_register_date: currentDate,
     };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    try {
+      // Criar aluno
+      console.log(
+        "Dados do aluno que estão sendo enviados: ",
+        updatedStudentData
+      );
+      const student = await createStudent(updatedStudentData);
+      setStudentId(student.id_student);
 
-        if (!validateEmail(studentData.student_email)) {
-            toast.error('Por favor, insira um e-mail válido antes de enviar.'); // Mensagem de erro
-            return; // Impede o envio se o e-mail for inválido
+      // Criar endereço associado ao aluno
+      console.log("Dados de localização que estão sendo enviados: ", {
+        ...locationData,
+        id_student: student.id_student,
+      });
+
+      await createLocation({
+        ...locationData,
+        id_student: student.id_student,
+      });
+
+      console.log("Curso selecionado: ", courseData);
+      console.log(
+        "Dados de associação aluno-curso que estão sendo enviados: ",
+        {
+          conclusion_date: studentCourseData.conclusion_date,
+          id_student: student.id_student,
+          id_course: courseData.id_course,
         }
+      );
 
-        // Registrar a data atual no formato ISO
-        const currentDate = new Date().toISOString();
+      // Verifica se há cursos associados
+      console.log(
+        "studentCourseData: " + JSON.stringify(studentCourseData, null, 2)
+      );
+      if (Array.isArray(courseData) && courseData.length > 0) {
+        for (let i = 0; i < courseData.length; i++) {
+          const course = courseData[i];
+          const conclusionDate = studentCourseData[i]?.conclusion_date;
 
-        // Atualizar o estado com a data de registro do aluno
-        const updatedStudentData = {
-            ...studentData,
-            student_register_date: currentDate
-        };
+          if (course && conclusionDate) {
+            console.log("Course data: ", JSON.stringify(course, null, 2)); 
+            console.log("Conclusion date: ", conclusionDate);
 
-        try {
-            // Criar aluno
-            console.log("Dados do aluno que estão sendo enviados: ", updatedStudentData);
-            const student = await createStudent(updatedStudentData);
-            setStudentId(student.id_student);
-
-            // Criar endereço associado ao aluno
-            console.log("Dados de localização que estão sendo enviados: ", {
-                ...locationData,
-                id_student: student.id_student
-            });
-
-            await createLocation({
-                ...locationData,
-                id_student: student.id_student,
-            });
-
-            console.log("Curso selecionado: ", courseData);
-            console.log("Dados de associação aluno-curso que estão sendo enviados: ", {
-                conclusion_date: studentCourseData.conclusion_date,
-                id_student: student.id_student,
-                id_course: courseData.id_course
-            });
-
-            // Criar a associação entre aluno e curso
             await createStudentCourse({
-                conclusion_date: studentCourseData.conclusion_date,
-                id_student: student.id_student,
-                id_course: courseData.id_course
+              conclusion_date: conclusionDate, // Data de conclusão do curso
+              id_student: student.id_student, // ID do aluno
+              id_course: course.id_course, // ID do curso
             });
-
-            toast.success('Dados criados com sucesso!');
-        } catch (error) {
-            console.error('Erro ao criar dados: ', error);
-            toast.error('Erro ao criar os dados');
+          } else {
+            console.error(`Missing course or conclusion date for index ${i}`);
+          }
         }
-    };
+      } else {
+        toast.error("Nenhum curso associado ao aluno.");
+      }
 
-    const handleDelete = async () => {
-        if (!studentId) {
-            toast.error('Nenhum aluno encontrado para deletar.');
-            return;
-        }
+      toast.success("Dados criados com sucesso!");
+    } catch (error) {
+      console.error("Erro ao criar dados: ", error);
+      toast.error("Erro ao criar os dados");
+    }
+  };
 
-        try {
-            await deleteAllStudentData(studentId);
-            setStudentData({ // Limpar os dados do aluno
-                student_name: '',
-                student_lastname: '',
-                student_birthdate: '',
-                student_cpf: '',
-                student_gender: '',
-                student_email: '',
-                student_register_date: ''
-            });
-            setLocationData({ // Limpar os dados do endereço
-                cep: '',
-                country: '',
-                street: '',
-                district: '',
-                number: '',
-                complement: '',
-                city: '',
-                state: ''
-            });
-            setStudentCourseData({ // Limpar a data de conclusão
-                conclusion_date: ''
-            });
+  const handleDelete = async () => {
+    if (!studentId) {
+      toast.error("Nenhum aluno encontrado para deletar.");
+      return;
+    }
 
-            setStudentId(null); // Resetar o ID do aluno
-            toast.success('Dados deletados com sucesso!');
-        } catch (error) {
-            console.error('Erro ao deletar dados:', error);
-            toast.error('Erro ao deletar os dados!');
-        }
-    };
+    try {
+      await deleteAllStudentData(studentId);
+      setStudentData({
+        // Limpar os dados do aluno
+        student_name: "",
+        student_lastname: "",
+        student_birthdate: "",
+        student_cpf: "",
+        student_gender: "",
+        student_email: "",
+        student_register_date: "",
+      });
+      setLocationData({
+        // Limpar os dados do endereço
+        cep: "",
+        country: "",
+        street: "",
+        district: "",
+        number: "",
+        complement: "",
+        city: "",
+        state: "",
+      });
+      setStudentCourseData({
+        // Limpar a data de conclusão
+        conclusion_date: "",
+      });
 
-    return (
-        <div>
-            <DynamicHeader
-                showLogo={false}
-                backIcon={() => navigate('/')}
-                studentName=""
-                onDelete={handleDelete}
-            />
-            <StudentForm
-                studentData={studentData}
-                setStudentData={setStudentData}
-                setEmailValid={setEmailValid}
-            />
-            <LocationForm
-                locationData={locationData}
-                setLocationData={setLocationData}
-            />
-            <CourseForm
-                courseData={courseData}
-                setCourseData={setCourseData}
-                studentCourseData={studentCourseData}
-                setStudentCourseData={setStudentCourseData}
-            />
-            <div className="d-flex justify-content-center mt-3">
-                <CustomButton type="submit" onClick={handleSubmit} className="btn mt-3">
-                    Salvar
-                </CustomButton>
-            </div>
-            <ToastContainer />
-        </div>
-    );
-};
+      setStudentId(null); // Resetar o ID do aluno
+      toast.success("Dados deletados com sucesso!");
+    } catch (error) {
+      console.error("Erro ao deletar dados:", error);
+      toast.error("Erro ao deletar os dados!");
+    }
+  };
+
+  return (
+    <div>
+      <DynamicHeader
+        showLogo={false}
+        backIcon={() => navigate("/")}
+        studentName=""
+        onDelete={handleDelete}
+      />
+      <StudentForm
+        studentData={studentData}
+        setStudentData={setStudentData}
+        setEmailValid={setEmailValid}
+      />
+      <LocationForm
+        locationData={locationData}
+        setLocationData={setLocationData}
+      />
+      <CourseForm
+        courseData={courseData}
+        setCourseData={setCourseData}
+        studentCourseData={studentCourseData}
+        setStudentCourseData={setStudentCourseData}
+      />
+      <div className="d-flex justify-content-center mt-3">
+        <CustomButton type="submit" onClick={handleSubmit} className="btn mt-3">
+          Salvar
+        </CustomButton>
+      </div>
+      <ToastContainer />
+    </div>
+  );
+}
 
 export default LayoutForm;
